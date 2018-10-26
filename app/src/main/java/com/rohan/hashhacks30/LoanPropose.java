e6383b22d52b44eef45e482058c8cc7e3590c3d1
vinamra bathwal <vinamrabathwal99@gmail.com>
2018-10-27 00:51:00 +0530

    intermediate

--- app/src/main/java/com/rohan/hashhacks30/LoanPropose.java
+++ app/src/main/java/com/rohan/hashhacks30/LoanPropose.java
@@ -2,12 +2,24 @@ package com.rohan.hashhacks30;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoanPropose extends AppCompatActivity {
+TextView Emi;
+EditText amount;
+String loanAmount;
 
+EditText interestRate;
+EditText numMonths;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_propose);
+        amount=findViewById(R.id.loanamount);
+        interestRate=findViewById(R.id.emi);
+        numMonths=findViewById(R.id.time);
+        Emi=findViewById(R.id.monthlyPay);
+
     }
 }
