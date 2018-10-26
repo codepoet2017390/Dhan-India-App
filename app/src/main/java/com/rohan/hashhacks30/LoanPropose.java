4abb2299b6fec41335a98ad1401380f2c273bc74
vinamra bathwal <vinamrabathwal99@gmail.com>
2018-10-27 01:57:16 +0530

    dwd

--- app/src/main/java/com/rohan/hashhacks30/LoanPropose.java
+++ app/src/main/java/com/rohan/hashhacks30/LoanPropose.java
@@ -2,14 +2,18 @@ package com.rohan.hashhacks30;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoanPropose extends AppCompatActivity {
 TextView Emi;
 EditText amount;
-String loanAmount;
-
+int loanAmount;
+int rate;
+int timeMon;
+int total;
 EditText interestRate;
 EditText numMonths;
     @Override
@@ -20,6 +24,38 @@ EditText numMonths;
         interestRate=findViewById(R.id.emi);
         numMonths=findViewById(R.id.time);
         Emi=findViewById(R.id.monthlyPay);
+//        loanAmount=Integer.parseInt(amount.getText().toString());
+//        rate=Integer.parseInt(interestRate.getText().toString());
+//        timeMon=Integer.parseInt(numMonths.getText().toString());
+//        total=loanAmount+loanAmount*rate/100;
+//        total=total/timeMon;
+//        Emi.setText("Your Emi is -"+total);
+      numMonths.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+              if(s.length()!=0)
+              {
+        loanAmount=Integer.parseInt(amount.getText().toString());
+        rate=Integer.parseInt(interestRate.getText().toString());
+        timeMon=Integer.parseInt(numMonths.getText().toString());
+        total=loanAmount+loanAmount*rate/100;
+        total=total/timeMon;
+        Emi.setText("Your Emi is - "+total);
+              }
+            }
+        });
 
     }
+
+
 }
