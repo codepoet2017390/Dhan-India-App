b7e204c8a923e67bd658ca337933dddd60cc7d0d
vinamra bathwal <vinamrabathwal99@gmail.com>
2018-10-27 05:46:58 +0530

    dssdd

--- app/src/main/java/com/rohan/hashhacks30/MainActivity.java
+++ app/src/main/java/com/rohan/hashhacks30/MainActivity.java
@@ -17,75 +17,72 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_activity);
-        private void setUpNavigationView() {
-            //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
-            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+       setUpNavigationView();
+    }
+    private void setUpNavigationView(){
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
-                // This method will trigger on item Click of navigation menu
-                @Override
-                public boolean onNavigationItemSelected(MenuItem menuItem) {
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
 
-                    //Check to see which item was being clicked and perform appropriate action
-                    switch (menuItem.getItemId()) {
-                        //Replacing the main content with ContentFragment Which is our Inbox View;
-                        case R.id.nav_home:
-                            navItemIndex = 0;
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+                        navItemIndex = 0;
 //                        CURRENT_TAG = TAG_HOME;
-                            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                                drawer.closeDrawers();
-                            }
-                            break;
+                        if (drawer.isDrawerOpen(GravityCompat.START)) {
+                            drawer.closeDrawers();
+                        }
+                        break;
                     /*case R.id.nav_notifichghgations:
                         navItemIndex = 1;
                         CURRENT_TAG = TAG_NOTIFICATIONS;
                         break;*/
-                        case R.id.nav_myaccount:
-                            navItemIndex = 2;
-                            //CURRENT_TAG = TAG_SETTINGS;
-                            Intent intent = new Intent(MainActivity.this, PersonInfo.class);
-                            startActivity(intent);
-                            return true;
+                    case R.id.nav_myaccount:
+                        navItemIndex = 2;
+                        //CURRENT_TAG = TAG_SETTINGS;
+                        Intent intent = new Intent(MainActivity.this, PersonInfo.class);
+                        startActivity(intent);
+                        return true;
 
-                        case R.id.nav_about_us:
-                            // launch new intent instead of loading fragment
-                            startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
-                            return true;
-                        case R.id.nav_privacy_policy:
-                            // launch new intent instead of loading fragment
-                            startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
-                            drawer.closeDrawers();
-                            return true;
-                        case R.id.nav_log_out :
-                            Toast.makeText(getApplicationContext(), "User logged out", Toast.LENGTH_LONG).show();
-                            Intent intent3 = new Intent(MainActivity.this, LoginActivity.class);
-                            intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(intent3);
-                            return true;
+                    case R.id.nav_Community_chat:
+                        // launch new intent instead of loading fragment
+                        startActivity(new Intent(MainActivity.this, MainChatActivity.class));
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_ask:
+                        Toast.makeText(getApplicationContext(), "User logged out", Toast.LENGTH_LONG).show();
+                        Intent intent3 = new Intent(MainActivity.this, LoanPropose.class);
+                        intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent3);
+                        return true;
 
 
-                        case R.id.nav_contact_us :
-                            viewPager.setCurrentItem(2);
-                            // Intent intent2 = new Intent(MainActivity.this, PopSearch.class);
-                            // startActivity(intent2);
-                            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                                drawer.closeDrawers();
-                            }
-                            return true;
+//                        case R.id.nav_contact_us :
+//                            viewPager.setCurrentItem(2);
+//                            // Intent intent2 = new Intent(MainActivity.this, PopSearch.class);
+//                            // startActivity(intent2);
+//                            if (drawer.isDrawerOpen(GravityCompat.START)) {
+//                                drawer.closeDrawers();
+//                            }
+//                            return true;
 
-                        default:
-                            navItemIndex = 0;
-                    }
-
-                    //Checking if the item is in checked state or not, if not make it in checked state
-                    if (menuItem.isChecked()) {
-                        menuItem.setChecked(false);
-                    } else {
-                        menuItem.setChecked(true);
-                    }
-                    menuItem.setChecked(true);
-                    //loadHomeFragment();
-                    return true;
+                    default:
+                        navItemIndex = 0;
                 }
-            });
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+                //loadHomeFragment();
+                return true;
+            }
+        });
     }
 }
